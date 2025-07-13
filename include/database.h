147a84180957c37e30c0e25c2dc802e064b4e82d
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-07-13 20:15:04 +0100

    megrate to sqlite

---
+++ include/database.h
@@ -0,0 +1,14 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <sqlite3.h>
+
+#define DEFAULT_DB_PATH "data/tasks.db"
+
+
+
+
+sqlite3* db_open(const char* filename);
+int db_init_schema(sqlite3* db);
+
+#endif
---
+++ src/database.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "database.h"
+
+sqlite3* db_open(const char* filename){
+    sqlite3 *db;
+    
+    int rc = sqlite3_open(filename, &db);
+
+    if (rc != SQLITE_OK){
+        fprintf(stderr, "Cannot open database %s\n", sqlite3_errmsg(db));
+        return NULL;
+    }
+    printf("the db is secesfuly buildet\n");
+    return db;
+}
+
+int db_init_schema(sqlite3* db){
+    const char *create_table_sql = 
+    "CREATE TABLE IF NOT EXISTS tasks ("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "title TEXT NOT NULL,"
+    "status INTEGER DEFAULT 0,"
+    "due_date INEGER,"
+    "created_at INTEGER DEFAULT (strftime('%s', 'now')),"
+    "updated_at INTEGER DEFAULT (strftime('%s', 'now'))"
+    ");";
+
+    char* err_msg = NULL;
+    int rc = sqlite3_exec(db, create_table_sql, NULL, NULL, &err_msg);
+    if (rc != SQLITE_OK){
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        return -1;
+    }
+    return 0;
+}
+
