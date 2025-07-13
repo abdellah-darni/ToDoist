56085c800b9e52e4e333aac51a486eea21552725
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-07-13 20:38:42 +0100

    i ment this

--- src/database.c
+++ src/database.c
@@ -27,13 +27,30 @@ int db_init_schema(sqlite3* db){
     "updated_at INTEGER DEFAULT (strftime('%s', 'now'))"
     ");";
 
-    char* err_msg = NULL;
+    char *err_msg = NULL;
     int rc = sqlite3_exec(db, create_table_sql, NULL, NULL, &err_msg);
     if (rc != SQLITE_OK){
         fprintf(stderr, "SQL error: %s\n", err_msg);
         sqlite3_free(err_msg);
         return -1;
     }
+    return 0;
+}
+
+int is_table_exist(sqlite3* db){
+    const char *check_sql =
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='tasks';";
+    
+    sqlite3_stmt *stmt;
+    
+    int rc = sqlite3_prepare_v2(db, check_sql, -1, &stmt, NULL);
+
+    if (rc == SQLITE_OK) {
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            return 1;
+        }
+    }
+    sqlite3_finalize(stmt); 
     return 0;
 }
 
