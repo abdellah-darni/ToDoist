d78c504cca72a4c828bc56d1ba3853a291af46fd
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-07-18 13:48:47 +0100

    change the db, add tags

--- src/database.c
+++ src/database.c
@@ -17,27 +17,37 @@ sqlite3* db_open(const char* filename){
 }
 
 int db_init_schema(sqlite3* db){
-    const char *create_table_sql = 
-    "CREATE TABLE IF NOT EXISTS tasks ("
-    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-    "title TEXT NOT NULL,"
-    "description TEXT,"
-    "status INTEGER DEFAULT 0,"
-    "due_date INEGER,"
-    "created_at INTEGER DEFAULT (strftime('%s', 'now')),"
-    "updated_at INTEGER DEFAULT (strftime('%s', 'now'))"
-    ");";
+    const char *create_tables_sql[] = {
+      "CREATE TABLE IF NOT EXISTS tasks ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "title TEXT NOT NULL,"
+        "description TEXT,"
+        "status INTEGER DEFAULT 0,"
+        "due_date INTEGER,"
+        "created_at INTEGER DEFAULT (strftime('%s','now')),"
+        "updated_at INTEGER DEFAULT (strftime('%s','now'))"
+      ");",
+      "CREATE TABLE IF NOT EXISTS tags ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "name TEXT NOT NULL UNIQUE"
+      ");",
+      "CREATE TABLE IF NOT EXISTS task_tags ("
+        "task_id INTEGER NOT NULL,"
+        "tag_id  INTEGER NOT NULL,"
+        "PRIMARY KEY (task_id, tag_id),"
+        "FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,"
+        "FOREIGN KEY (tag_id)  REFERENCES tags(id)  ON DELETE CASCADE"
+      ");"
+    };
 
     char *err_msg = NULL;
-    int rc = sqlite3_exec(db, create_table_sql, NULL, NULL, &err_msg);
-    if (rc != SQLITE_OK){
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        return -1;
+    for (int i = 0; i < 3; i++ ){
+        int rc = sqlite3_exec(db, create_tables_sql[i], NULL, NULL, &err_msg);
+        if (rc != SQLITE_OK){
+            fprintf(stderr, "SQL error: %s\n", err_msg);
+            sqlite3_free(err_msg);
+            return -1;
+        }
     }
     return 0;
 }
-
-
-
-
--- src/main.c
+++ src/main.c
@@ -1,84 +1,28 @@
-// // #define _XOPEN_SOURCE 700
-// #include <stdio.h>
+// #define _XOPEN_SOURCE 700
+#include <stdio.h>
 // #include <time.h>
 // #include <ncurses.h>
 
-// // #include "file_io.h"
 // // #include "todoist.h"
-// // #include "database.h"
-
-// void init_tui();
-
-// int main(){
-
-//     // sqlite3 *db = db_open(DEFAULT_DB_PATH);
-    
-
-//     // if (!db_init_schema(db)){
-//     //     printf("the table created\n");
-//     // }
-
-//     // char *title = "title";
-//     // char *desc = "desc 1";
-
-    
-//     // int rc = add_task(db,title, desc,1234567890);
-//     // printf("\n rc = %d \n",rc);
-
-//     // sqlite3_close(db);
-//     //while(1){
-//         // initscr();
-//         // printw("Hello World !!!");
-//         // refresh();
-//         // getch();
-//         // endwin();
-//     //}
-    
-//     init_tui();
+#include "database.h"
 
 
-//     return 0;
-// }
+int main(){
 
-// void init_tui() {
-//     printf("----------------1: befoure\n");
-//     initscr();          // Initialize the ncurses mode
-//     printf("----------------1: after\n");
-//     cbreak();           // Disable line buffering
-//     noecho();           // Don't echo input characters
 
-//     // Create a window with a height of 5 rows and a width of 30 columns
-//     WINDOW *down_bar = newwin(10, 30, 5, 10);
-    
-//     // Check if the window was created successfully
-//     if (down_bar == NULL) {
-//         endwin(); // End ncurses mode if window creation failed
-//         printf("Error creating window:\n");
-//         return;
-//     }
-    
-//     // Draw a box around the window with default characters
-//     box(down_bar, 0, 0); // 0, 0 uses default characters for borders
 
-//     // Print content inside the window
-//     printf("----------------2: befoure\n");
+    sqlite3 *db = db_open(DEFAULT_DB_PATH);
 
-//     mvwprintw(down_bar, 2, 1, "Window content"); // Centered text in the window
-    
-//     wrefresh(down_bar); // Refresh the window to show the changes
-//     printf("----------------2: after\n");
-//     getch(); // Wait for user input
-//     printf("----------------3: after\n");
-//     delwin(down_bar); // Delete the window
-//     endwin(); // End ncurses mode
-// }
+    if (db != NULL){
+        printf("yay\n");
+    }
 
-#include "tui.h"
+    if (!db_init_schema(db)){
+        printf("the table created\n");
+    }
 
-void init_tui();
+    sqlite3_exec();
 
-int main() {
-    init_tui();
     return 0;
 }
 
