1afc1a9f5f154e79f273dd9438b6b1f6814861aa
abdellah-darni <abdellah.darni.2018@gmail.com>
2025-07-19 23:02:18 +0100

    main for tests

--- src/main.c
+++ src/main.c
@@ -1,27 +1,39 @@
-// #define _XOPEN_SOURCE 700
-#include <stdio.h>
-// #include <time.h>
-// #include <ncurses.h>
 
-// // #include "todoist.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+
+// // // #include "todoist.h"
 #include "database.h"
+#include "tui.h"
+
 
 
 int main(){
 
-
-
     sqlite3 *db = db_open(DEFAULT_DB_PATH);
-
-    if (db != NULL){
-        printf("yay\n");
-    }
 
     if (!db_init_schema(db)){
         printf("the table created\n");
     }
 
+    int tags_count;
+    char **tags_list = NULL;
+
+    int state = load_tags(db, &tags_list, &tags_count);
+    printf("state : %d\n", state);
+    if (state != 0){
+        printf("shitt !!");
+        return 1;
+    }
+
+    for (int j = 0; j < tags_count; j++){
+        printf("- %s\n",tags_list[j]);
+    }
+
+    // init_tui();
+   
+
     return 0;
 }
-
-
